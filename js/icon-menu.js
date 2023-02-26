e1bf5ad48d0b78efbd76edf49fb3c0e006085504
Eric Asiedu <90698535+erickwekuasiedu@users.noreply.github.com>
2023-02-26 11:45:38 +0000

    Update icon-menu.js
    Code refactor

--- js/icon-menu.js
+++ js/icon-menu.js
@@ -2,97 +2,102 @@
 ICON-MENU V1.0 a fully customizable fontawsome menu bar with callbacks.
 Hey my name is Eric Asiedu, i created this template 
 for my personal site and thought it might be useful to you.
-Donation are welcome at erickwekuasiedu@gmail.com Thank you.
+Donation are welcome on paypal erickwekuasiedu@gmail.com Thank you.
 ----------------------------------------------------------------------------------------------------
 */
 
 //main function
-const makeBar=function(props){
-  //get the main container for this bar
-    const layer=document.querySelector('#'+props.layer);
-    
-    //this creates box for the icon
-    const makeBox=function(iconTag,id){
-      //set box propeties and add icon
-      const box=document.createElement('div');
-      box.classList.add('box');
-      box.style.width=props.boxSize;
-      box.style.height=props.boxSize;
-      box.style.backgroundColor=props.colorA;
-      iconTag.style.color=props.colorB;
-      box.appendChild(iconTag);
-      //set the css variable to be use for padding
-      const minus=(props.direction==='top' || props.direction==='left') ? '-':'';
-      const amount=props.stretchAmount || '50px';
-      box.style.setProperty("--s-pad",props.stretchAmount || '50px');
-      box.style.setProperty("--s-mag",minus+amount);
-      
-      //add mouse over the box
-      box.addEventListener('mouseenter',function(evnt){
-      
-        box.style.backgroundColor=props.colorB;
-        iconTag.style.color=props.colorA;
-        
-        if(props.direction=="left") this.classList.toggle('stretch-left');
-        if(props.direction=="right") this.classList.toggle('stretch-right');
-        if(props.direction=="top") this.classList.toggle('stretch-top');
-        if(props.direction=="bottom") this.classList.toggle('stretch-bottom');
-      });
-      
-      //add mouse out event to box
-      box.addEventListener('mouseleave',function(evnt){
-        box.style.backgroundColor=props.colorA;
-        iconTag.style.color=props.colorB;
-        
-        if(props.direction=="left") this.classList.toggle('stretch-left');
-        if(props.direction=="right") this.classList.toggle('stretch-right');
-        if(props.direction=="top") this.classList.toggle('stretch-top');
-        if(props.direction=="bottom") this.classList.toggle('stretch-bottom');
-      });
-      //call click function for each icon
-      if(props.clicks){
-          box.addEventListener('click',function(evnt){
-          props.clicks[id]();
-         })
-      }
-      
-      return box;
-    }
-    
-    const isRow=(props.direction==='top' || props.direction==='bottom');
-    if(isRow) layer.classList.toggle('flex-row');
-    
-     
-    //loop throug icons list and create
-    for(let i=0;i<props.icons.length;i++){
-      
-      
-      //link decoration
-      const linkTag=document.createElement('a');
-      if(props.hrefs) linkTag.href=props.hrefs[i];
-     
-      linkTag.style.textDecoration="none";
-      //icon decoration
-      const iconTag=document.createElement('i');
-      iconTag.style.fontSize=props.fontSize;
-      iconTag.style.transition=props.transition
-      
-      //loop through each icon string and add class
-      for(let c of props.icons[i].split(" ")){
-        iconTag.classList.add(c);
-      }
-      
-      //create a box with icon and aling
-      const box=makeBox(iconTag,i);
-      if(props.direction==='top') box.classList.toggle('top');
-      if(props.direction==='bottom') box.classList.toggle('bottom');
-      
-      //add the box with a link to container
-      linkTag.appendChild(box); 
-      layer.appendChild(linkTag);
+const makeBar = function(options) {
   
+  //
+  const layer = document.querySelector('#'+options.layer);
+  
+  //set box propeties and add icon
+  const makeBox = function(iconTag, id) {
+    
+    //
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width  = options.boxSize;
+    box.style.height = options.boxSize;
+    box.style.backgroundColor = options.colorA;
+    
+    //
+    iconTag.style.color = options.colorB;
+    box.appendChild(iconTag);
+    
+    //set the css variable to be use for padding
+    const minus  = (options.direction === 'top' || options.direction === 'left') ? '-' : '';
+    const amount = options.stretchAmount || '50px';
+    box.style.setProperty("--s-pad", options.stretchAmount || '50px');
+    box.style.setProperty("--s-mag", minus+amount);
+
+    //
+    box.addEventListener('mouseenter', function(evnt) {
+      
+      //
+      box.style.backgroundColor = options.colorB;
+      iconTag.style.color = options.colorA;
+      
+      //
+      this.classList.toggle('stretch-' + options.direction);
+    
+    });
+
+    //add mouse out event to box
+    box.addEventListener('mouseleave', function(evnt) {
+
+      //
+      box.style.backgroundColor = options.colorA;
+      iconTag.style.color = options.colorB;
+      
+      //
+      this.classList.toggle('stretch-' + options.direction);
+    
+    });
+    
+    //call click function for each icon
+    if (options.clicks) {
+        box.addEventListener('click', function(evnt) {
+          options.clicks[id]();
+       });
     }
+
+    return box;
+  
   }
+
+  //
+  if ((options.direction === 'top' || options.direction === 'bottom')) {
+    layer.classList.toggle('flex-row');
+  }
+
+  //loop throug icons list and create
+  for (let i=0; i<options.icons.length; i++) {
+    
+    //link decoration
+    const linkTag = document.createElement('a');
+    if (options.hrefs) linkTag.href = options.hrefs[i];
+    linkTag.style.textDecoration = "none";
+    
+    //icon decoration
+    const iconTag = document.createElement('i');
+    iconTag.style.fontSize = options.fontSize;
+    iconTag.style.transition = options.transition;
+
+    //loop through each icon string and add class
+    for (let c of options.icons[i].split(" ")) {
+      iconTag.classList.add(c);
+    }
+
+    //create a box with icon and aling
+    const box = makeBox(iconTag,i);
+    box.classList.toggle(options.direction);
   
-  
-  
+    //add the box with a link to container
+    linkTag.appendChild(box); 
+    layer.appendChild(linkTag);
+
+  }
+
+}
